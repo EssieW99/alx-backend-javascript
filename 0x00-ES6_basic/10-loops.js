32094500fab0e3abb68de324942187e0a7bea9a1
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-06-25 16:53:00 +0300

    10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (let item of array) {
+    item = appendString + item;
+    newArray.push(item);
+  }
+
+  return newArray;
+}
