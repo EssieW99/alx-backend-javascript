30cc2e213811f779a3a91baa3169a19034c8b1e0
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-06-25 10:59:30 +0300

    Fix: lint error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  
-    if (trueOrFalse) {
-      var task = true;
-      var task2 = false;
-    }
-  
-    var task = false;
-    var task2 = true;
-    return [task, task2];
+  let task;
+  let task2;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
   }
+
+  task = false;
+  task2 = true;
+  return [task, task2];
+}
