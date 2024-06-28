b2a1ae3eaea185755cece6ecebd07cddc887c5c8
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-06-28 15:38:46 +0300

    2. A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  /* getter for the name attribute */
+  get name() {
+    return this._name;
+  }
+
+  /* setter for the name attribute */
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  /* getter for the length attribute */
+  get length() {
+    return this._length;
+  }
+
+  /* setter for the length attribute */
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  /* getter for the students attribute */
+  get students() {
+    return this._students;
+  }
+
+  /* setter for the students attribute */
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students value must be an array');
+    }
+    for (const item of value) {
+      if (typeof item !== 'string') {
+        throw new TypeError('Students elements in the array must be strings');
+      }
+    }
+    this._students = value;
+  }
+}
