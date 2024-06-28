710e1e502ae850299ebe5d59ab85c3b57d951175
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-06-28 16:10:29 +0300

    3. Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  /* getter for the code attribute */
+  get code() {
+    return this._code;
+  }
+
+  /* setter for the code attribute */
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  /* getter for the name attribute */
+  get name() {
+    return this._name;
+  }
+
+  /* setter for the name attribute */
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code}) `;
+  }
+}
