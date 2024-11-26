7485e0fed4a6a7b49c53393df79c196cf0502e7d
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-11-26 17:38:03 +0300

    5. Create a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const port = 1245
+
+const app = http.createServer(async (req, res) => {
+  if (req.url == '/') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Holberton School!');
+  }else if (req.url == '/students') {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    let txt = 'This is the list of our students\n';
+    try {
+      const msg = await countStudents(process.argv[2]);
+      txt += msg;
+    } catch (err) {
+      txt += err.message;
+    }
+    res.end(txt);
+  }
+ 
+});
+app.listen(port);
+
+module.exports = app
