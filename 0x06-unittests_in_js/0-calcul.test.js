522acfe60f5c22f3eb366abd03c860cf6d52fb30
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-11-28 19:08:48 +0300

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,16 +1,16 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('returns the sum of two integers', function () {
-    assert.strictEqual(calculateNumber(1, 2), 3);
+describe('calculateNumber', function() {
+  it('returns the sum of two integers', function() {
+    assert.equal(calculateNumber(1, 2), 3);
   });
 
-  it('returns the sum with one rounded integer', function () {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('returns the sum with one rounded integer', function() {
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
 
-  it('returns the sum of two rounded integers', function () {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('returns the sum of two rounded integers', function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
