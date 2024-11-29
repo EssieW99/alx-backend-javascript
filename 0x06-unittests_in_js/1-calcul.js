3f8eabbba3a6b207c20b4309bd0196cd3378ff29
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-11-29 20:57:46 +0300

    1. Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,17 @@
+function calculateNumber(type, a, b) {
+  a = Math.round(a);
+  b = Math.round(b);
+  // check the operation
+  if (type === 'SUM') {
+    return a + b;
+  } else if (type === 'SUBTRACT') {
+    return a - b;
+  } else if (type === 'DIVISION') {
+    if (b === 0) {
+      return 'Error';
+    }
+    return a / b;
+  } 
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+  it('returns the sum of two integers', function() {
+    assert.equal(calculateNumber('SUM', 2, 3), 5);
+  });
+
+  it('returns the sum of two decimals', function() {
+    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+
+  it('returns the result after subtracting whole integers', function() {
+    assert.equal(calculateNumber('SUBTRACT', 6, 3), 3);
+  });
+
+  it('returns the result after subtracting two decimals', function() {
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+
+  it('returns the result after the division of two whole integers', function() {
+    assert.equal(calculateNumber('DIVISION', 8, 4), 2);
+  });
+
+  it('returns the result after dividing two integers', function() {
+    assert.equal(calculateNumber('DIVISION', 1.4, 4.5), 0.2)
+  });
+
+  it('returns the string Error when b is zero', function() {
+    assert.equal(calculateNumber('DIVISION', 5, 0), 'Error');
+  });
+});
